8b30fba7e221632a7d1036c5669bd782f2b3cce4
oleragnar-boop <olerr@stud.ntnu.no>
2022-05-08 17:22:05 +0200

    changes to app

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const app = express();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Humidity = require('./models/humidity_schema')
+const LightSensorModel = require('./models/light_schema')
+const TemperatureSensorModel = require('./models/temp_schema')
 const db = mongoose.connection;
 
 mongoose.connect("mongodb+srv://admin:adminadmin@data.c8vtj.mongodb.net/Smart_greenhouse?retryWrites=true&w=majority", {
 	useUnifiedTopology: true,
 	useNewUrlParser: true
-});
+})
+.then(mongoose => {
 
-app.set('views',__dirname+'/views');
+    app.set('views',__dirname+'/views');
  app.set('view engine','ejs');
  app.use(bodyParser.urlencoded({ extended: true }))
 
+ app.listen(process.env.PORT || 3000, function () {
+    console.log('listening on 3000')
+  })
+
 
 app.get('/', (req, res) => {
-    db.collection('humidity_data').findOne().sort({created_at: -1}).toArray()
-    .then(results => {
+    db.collection('humidity_data').findOne()
+    .then(results => { 
         let humData = results;
-        db.collection('humidity_data').findOne().sort({created_at: -1}).toArray()
-        
+        console.log(results)
+        db.collection('lightSensor_data').findOne()
+        .then(results => {
+            let lightData = results;
+            console.log(results)
+            db.collection('temperatureSensor_data').findOne()
+            .then(results => {
+                let tempData = results;
+                console.log(results)
+                res.render("index.ejs", { humData: humData, lightData: lightData, tempData: tempData});
+            })
+        })
     })
-    res.render(__dirname + "index.ejs");
+})
+
+
 })
--- models/temp_schema.js
+++ models/temp_schema.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const TemperatureSensorSchema = new mongoose.Schema({
     name: { type: String, required: true},
-    time: { type: Date, required: true, default: Date.now() },
-    temperature: { type: String, required: true, default: 'celsius' },
+    time: { type: Date, required: true},
+    temperature: { type: String, required: true},
 });
 
 const TemperatureSensorModel = mongoose.model("TemperatureSensorModel", TemperatureSensorSchema, "temperatureSensor_data");
